edb462af79210ca6ffe8f6ff3068bf3f90b64219
Rafiul <rafiulkarim51@gmail.com>
2021-06-22 03:29:54 +0600

    product modal add

--- resources/views/layouts/frontlayout.blade.php
+++ resources/views/layouts/frontlayout.blade.php
@@ -462,6 +462,8 @@
 <script src="{{asset("resources/frontend/form-wizard/vendor/wnumb/wNumb.js")}}"></script>
 <script src="{{asset("resources/frontend/form-wizard/js/main.js")}}"></script>
 <script src="{{asset("resources/js/jquery.lazy.min.js")}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 <!--<script src="https://starceramicsbd.com/assets/frontend/js/jQuery.scrollSpeed.js"></script>-->
 <!--/form-wizard-->
 
--- resources/views/wall.blade.php
+++ resources/views/wall.blade.php
@@ -5,6 +5,13 @@
         .transparent-header{
             background-color: #00aced;
         }
+        .product-details{
+            margin-bottom: 0;
+        }
+        .product-info{
+            font-size: 14px;
+            margin: 5px  10px 0 10px;
+        }
     </style>
 
 @endsection
@@ -16,50 +23,110 @@
 @section("content")
     <div class="pg-contact-details bg-grey section-padding-sm mobile-mt-0 mt-5">
         <div class="container">
-            <h5 class="text-center"><b>Wall Tiles</b></h5>
+            <h2 class="text-center">Walls Tiles</h2>
             <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-4 col-sm-4 col-12 mt-2 product-card">
-                    <div class="single-quick-contact service-style-2 text-center " id="product">
-                        <a href="#"><img src="{{url('resources/images/product/wall/wall1.jpeg')}}" alt=""></a>
-                        <div id="one" style="display: none;">
-                            <p><u><b>Tiles Info </b></u></p>
-                            <p>Category <b>:</b> Wall </p>
-                            <p>Model No <b>:</b> 215 </p>
-                            <p>Size <b>:</b> 300 X 800 mm </p>
-                            <p>Function <b>:</b> Waterproof, Wear Resistant, Anti-Static, Acid-Resistant </p>
-                            <p>Usage <b>:</b> Household, Interior & Indoor Tiles </p>
-                            <p>Material <b>:</b> Ceramic </p>
-                            <p>Surface Function <b>:</b> Glazed </p>
+                    <div class="service-style-2" id="product">
+                        <img src="{{url('resources/images/product/wall/wall1.jpeg')}}" data-toggle="modal" data-target="#exampleModal" alt="">
+
+                        <!-- Modal -->
+                        <div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Tiles Info </h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <img class="mx-sm-2 my-sm-2"  src="{{url('resources/images/product/wall/wall1.jpeg')}}"  alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="product-info">
+                                                <p class="product-details">Category <b>:</b> Wall </p>
+                                                <p class="product-details">Model No <b>:</b> 215 </p>
+                                                <p class="product-details">Size <b>:</b> 300 X 800 mm </p>
+                                                <p class="product-details">Function <b>:</b> Waterproof, Wear Resistant, Anti-Static, Acid-Resistant </p>
+                                                <p class="product-details">Usage <b>:</b> Household, Interior & Indoor Tiles </p>
+                                                <p class="product-details">Material <b>:</b> Ceramic </p>
+                                                <p class="product-details">Surface Function <b>:</b> Glazed </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-12 mt-2">
-                    <div class="single-quick-contact service-style-2 text-center " id="productOne">
-                        <a href="#"><img src="{{url('resources/images/product/wall/wall2.jpeg')}}" alt=""></a>
-                        <div id="two" style="display: none;">
-                            <p><u><b>Tiles Info </b></u></p>
-                            <p>Category <b>:</b> Wall </p>
-                            <p>Model No <b>:</b> 215 </p>
-                            <p>Size <b>:</b> 300 X 800 mm </p>
-                            <p>Function <b>:</b> Waterproof, Wear Resistant, Anti-Static, Acid-Resistant </p>
-                            <p>Usage <b>:</b> Household, Interior & Indoor Tiles </p>
-                            <p>Material <b>:</b> Ceramic </p>
-                            <p>Surface Function <b>:</b> Glazed </p>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-12 mt-2 product-card">
+                    <div class="service-style-2" id="product">
+                        <img src="{{url('resources/images/product/wall/wall2.jpeg')}}" data-toggle="modal" data-target="#exampleModal2" alt="">
+
+                        <!-- Modal -->
+                        <div class="modal fade bd-example-modal-lg" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Tiles Info </h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <img class="mx-sm-2 my-sm-2"  src="{{url('resources/images/product/wall/wall2.jpeg')}}"  alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div style="font-size: 14px; margin: 0 10px 0 10px;">
+                                                <p class="product-details">Category <b>:</b> Wall </p>
+                                                <p class="product-details">Model No <b>:</b> 215 </p>
+                                                <p class="product-details">Size <b>:</b> 300 X 800 mm </p>
+                                                <p class="product-details">Function <b>:</b> Waterproof, Wear Resistant, Anti-Static, Acid-Resistant </p>
+                                                <p class="product-details">Usage <b>:</b> Household, Interior & Indoor Tiles </p>
+                                                <p class="product-details">Material <b>:</b> Ceramic </p>
+                                                <p class="product-details">Surface Function <b>:</b> Glazed </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-12 mt-2">
-                    <div class="single-quick-contact service-style-2 text-center " id="productTwo">
-                        <a href="#"><img src="{{url('resources/images/product/wall/wall3.jpeg')}}" alt=""></a>
-                        <div id="three" style="display: none;">
-                            <p><u><b>Tiles Info </b></u></p>
-                            <p>Category <b>:</b> Wall </p>
-                            <p>Model No <b>:</b> 215 </p>
-                            <p>Size <b>:</b> 300 X 800 mm </p>
-                            <p>Function <b>:</b> Waterproof, Wear Resistant, Anti-Static, Acid-Resistant </p>
-                            <p>Usage <b>:</b> Household, Interior & Indoor Tiles </p>
-                            <p>Material <b>:</b> Ceramic </p>
-                            <p>Surface Function <b>:</b> Glazed </p>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-12 mt-2 product-card">
+                    <div class="service-style-2" id="product">
+                        <img src="{{url('resources/images/product/wall/wall3.jpeg')}}" data-toggle="modal" data-target="#exampleModal3" alt="">
+
+                        <!-- Modal -->
+                        <div class="modal fade bd-example-modal-lg" id="exampleModal3" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Tiles Info </h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <img class="mx-sm-2 my-sm-2"  src="{{url('resources/images/product/wall/wall3.jpeg')}}"  alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div style="font-size: 14px; margin: 0 10px 0 10px;">
+                                                <p class="product-details">Category <b>:</b> Wall </p>
+                                                <p class="product-details">Model No <b>:</b> 215 </p>
+                                                <p class="product-details">Size <b>:</b> 300 X 800 mm </p>
+                                                <p class="product-details">Function <b>:</b> Waterproof, Wear Resistant, Anti-Static, Acid-Resistant </p>
+                                                <p class="product-details">Usage <b>:</b> Household, Interior & Indoor Tiles </p>
+                                                <p class="product-details">Material <b>:</b> Ceramic </p>
+                                                <p class="product-details">Surface Function <b>:</b> Glazed </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -69,17 +136,5 @@
 @endsection
 
 @section("footer_script")
-    <script>
-        $(document).ready(function(){
-            $("#product").hover(function(){
-                $("#one").toggle();
-            });
-            $("#productOne").hover(function(){
-                $("#two").toggle();
-            });
-            $("#productTwo").hover(function(){
-                $("#three").toggle();
-            });
-        });
-    </script>
+
 @endsection
